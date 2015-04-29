776197598b845763caef7b099499c41450cc7cfb
jkr <joakim.karlsson@lundalogik.se>
2015-04-29 20:54:06 +0200

    Finds best matching local path in stack trace.
    Local paths, not including 'venv' or 'python' are preferred.

--- terseout.py
+++ terseout.py
@@ -1,5 +1,7 @@
 from nose.plugins import Plugin
-import traceback as t
+import traceback
+import os
+import re
 
 
 class TerseOutPlugin(Plugin):
@@ -7,7 +9,17 @@ class TerseOutPlugin(Plugin):
 
     def __init__(self):
         self.stream = None
+        self.basepath = os.getcwd()
         super().__init__()
+
+    def options(self, parser, env):
+        super().options(parser, env)
+        parser.add_option("--print-stack", action="store_true",
+                          default=False, help="Print stacktrace for errors")
+
+    def configure(self, options, conf):
+        self.print_stack = options.print_stack
+        super().configure(options, conf)
 
     def addError(self, test, err):
         self._report('error', test, err)
@@ -16,19 +28,32 @@ class TerseOutPlugin(Plugin):
         self._report('failure', test, err)
 
     def _report(self, err_or_failure, test, err):
-        error_type, error, traceback = err
-        self.stream.writeln('{location}: {message}'.format(
-            location=self._location(traceback),
-            message=str(error) or repr(error),
-        ))
+        error_type, error, tb = err
+        file_, line, *_ = self._first_local_stackframe(tb)
+        self.stream.writeln('{}:{}: {} {}'.format(
+            file_, line, test, str(error) or repr(error)))
 
-    def _location(self, traceback):
-        tb = t.extract_tb(traceback)
+        if self.print_stack:
+            self.stream.writeln(self._format_tb(tb))
 
-        selected = tb[-1]
+    def _format_tb(self, tb):
+        frames = traceback.extract_tb(tb)
+        return os.linesep.join(
+            ['    {file_}:{line}: {func} {statement}'.format(
+                file_=file_, line=line, func=func, statement=statement)
+             for file_, line, func, statement in frames]
+        )
 
-        file_, line, func, statement = selected
-        return '{}:{}'.format(file_, line)
+    def _first_local_stackframe(self, tb):
+        frames = traceback.extract_tb(tb)
+
+        def i_like_you(frame):
+            path, *_ = frame
+            return (path.startswith(self.basepath) and
+                    not re.search(r'python|venv', path, flags=re.IGNORECASE))
+
+        local_frames = filter(i_like_you, reversed(frames))
+        return next(local_frames, frames[-1])
 
     def setOutputStream(self, stream):
         class NullStream:
